3f752bdfbf51ae7fb2eb7425c0b1a5eccd48caf2
loder-coder <syinfo4@gmail.com>
2026-01-08 12:29:14 +0900

    엔진 test 2

--- app/services/saju_engine.py
+++ app/services/saju_engine.py
@@ -2,12 +2,13 @@ from lunar_python import Lunar
 
 
 def calculate_saju(lunar_year: int, lunar_month: int, lunar_day: int, hour: int, minute: int):
-    lunar = Lunar.fromYmd(lunar_year, lunar_month, lunar_day)
+    # 시간 포함해서 Lunar 생성
+    lunar = Lunar.fromYmdHms(lunar_year, lunar_month, lunar_day, hour, minute, 0)
     eight_char = lunar.getEightChar()
 
     return {
         "year": eight_char.getYearGan() + eight_char.getYearZhi(),
         "month": eight_char.getMonthGan() + eight_char.getMonthZhi(),
         "day": eight_char.getDayGan() + eight_char.getDayZhi(),
-        "time": eight_char.getTimeGanByTime(hour, minute) + eight_char.getTimeZhiByTime(hour, minute)
+        "time": eight_char.getTimeGan() + eight_char.getTimeZhi()
     }
